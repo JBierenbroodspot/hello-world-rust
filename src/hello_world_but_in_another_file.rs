1094f7bebe583849dbc7d127df368750b90cd28d
jimmy <jim12.brood@gmail.com>
2022-10-28 22:09:24 +0200

    Add documentation

--- src/hello_world_but_in_another_file.rs
+++ src/hello_world_but_in_another_file.rs
@@ -1,8 +1,12 @@
+//! This module contains a function to print `Hello, world!` to the console and
+//! a function to run said function.
+
+/// Runs the module.
 pub fn run_self() {
     print_hello_world();
 }
 
-/// Prints Hello, world! to console
+/// Prints Hello, world! to console.
 fn print_hello_world() {
     println!("Hello, world!");
 }

0386947e53882a343f7b0d6d6b1d23fc47274cdd
jimmy <jim12.brood@gmail.com>
2022-10-28 23:49:20 +0200

    Add function selection functionality

--- src/main.rs
+++ src/main.rs
@@ -13,9 +13,33 @@ fn main() -> io::Result<()> {
 
         match stdin.read_line(&mut user_input) {
             Err(_) => stop = true,
-            Ok(_) => println!("{}", user_input)
+            Ok(_) => stop = run_selection(&user_input)
         }
     }
 
     Ok(())
+}
+
+/// Takes a `&str` and runs a function based on specific matching values.
+/// 
+/// This function will match a borrowed `str` to hardcoded options and run a 
+/// function if there's a match.
+/// 
+/// # Arguments
+/// 
+/// * `selection` - A borrowed string to match against the existing options.
+/// 
+/// # Return value
+/// 
+/// If a function is successfully executed `true` is returned, otherwise `false`
+/// is returned.
+fn run_selection(selection: &str) -> bool {
+
+    // String returned by `read_line()` include a trailing newline character.
+    match selection.trim() {
+        "1" => hello_world_but_in_another_file::run_self(),
+        _   => return false
+    }
+
+    return true;
 }

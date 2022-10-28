8d5205d67155552597dc126bfa0e99d0d9408b9f
jimmy <jim12.brood@gmail.com>
2022-10-28 23:01:29 +0200

    Add taking of user input

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,21 @@
+use std::io;
+
 pub mod hello_world_but_in_another_file;
 
-fn main() {
-    hello_world_but_in_another_file::run_self();
+fn main() -> io::Result<()> {
+    let mut stop: bool = false;
+    let mut user_input: String;
+    let mut stdin: io::Stdin;
+    
+    while stop != true {
+        stdin = io::stdin();
+        user_input = String::new();
+
+        match stdin.read_line(&mut user_input) {
+            Err(_) => stop = true,
+            Ok(_) => println!("{}", user_input)
+        }
+    }
+
+    Ok(())
 }

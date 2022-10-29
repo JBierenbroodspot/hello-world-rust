6aa44c91d29a99ae1cbef88c05a605e18077fc10
jimmy <jim12.brood@gmail.com>
2022-10-29 19:38:39 +0200

    Add asking user for guess

--- src/guess_the_number.rs
+++ src/guess_the_number.rs
@@ -45,13 +45,14 @@ fn generate_random_number() -> u8 {
 /// guess = None;
 /// evaluate_guess(&guess, &to_guess); // -> (true, true)
 /// ```
-fn evaluate_guess(guess: Option<&u8>, to_guess: &u8) -> (bool, bool) {
-    return guess.map(|num| (num < to_guess, num > to_guess))
+fn evaluate_guess(guess: Option<u8>, to_guess: &u8) -> (bool, bool) {
+    return guess.map(|num| (num < *to_guess, num > *to_guess))
                 .or(Some((true, true)))
                 // Won't panic because this will always evaluate to `Some()`.
                 .unwrap();
 }
 
+// TODO(JBierenbroodspot): update documentation.
 /// Converts string to a unsigned 8 bit integer.
 /// 
 /// # Arguments
@@ -61,22 +62,34 @@ fn evaluate_guess(guess: Option<&u8>, to_guess: &u8) -> (bool, bool) {
 /// 
 /// # Return value
 /// 
-/// If `guess` can be succesfully parsed into a unsigned 8 bit integer then 
+/// If `guess` can be successfully parsed into a unsigned 8 bit integer then 
 /// `Some(u8)` will be returned, otherwise `None` is returned.
-fn guess_to_u8(guess: Option<&str>) -> Option<u8> {
-    return guess.map(|s| s.parse::<u8>())?
-                .ok();
+fn guess_to_u8(guess: &str) -> Option<u8> {
+    return guess.to_string().parse::<u8>().ok();
 }
 
 pub fn run_self() {
     let stdin: io::Stdin = io::stdin();
+    let to_guess: u8 = generate_random_number();
 
     let mut stop: bool = false;
-    let mut user_input: String;
+    let mut user_input: String = String::new();
+    let mut result: (bool, bool);
+    let mut guess: Option<u8>;
     
     while stop != true {
-        user_input = String::new();
+        user_input.clear();
 
-        
+        println!("Enter your guess ({} - {}):", u8::MIN, u8::MAX);
+
+        guess = stdin.read_line(&mut user_input)
+                     .ok()
+                     .and(guess_to_u8(&(user_input.trim())));
+
+        result = evaluate_guess(guess, &to_guess);
+
+        println!("The result is {:?}", result);
+        println!("The random number is {}", to_guess);
+        stop = true;
     }
 }

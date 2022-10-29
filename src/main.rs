7507f64604197a3117ab162ffd926ac29f69e8a9
jimmy <jim12.brood@gmail.com>
2022-10-29 14:49:12 +0200

    Add get_from_fn_map  - Remove get_selection  - Update comments

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,47 @@
 use std::io;
+use std::collections;
 
 mod hello_world_but_in_another_file;
+// Use an alias because the name is really long.
+use hello_world_but_in_another_file::run_self as hwbiaf;
 mod guess_the_number;
 
-/// Takes a `&str` and runs a function based on specific matching values.
+type FunctionMap = collections::HashMap<String, fn()>;
+
+// TODO(JBierenbroodspot): Remove redundant Option.
+/// Tries to get a key value from a `FunctionMap`.
 /// 
-/// This function will match a borrowed `str` to hardcoded options and run a 
-/// function if there's a match.
+/// This function takes a borrowed `str` and tries to get the value from a 
+/// `FunctionMap`.
 /// 
 /// # Arguments
 /// 
-/// * `selection` - A borrowed string to match against the existing options.
+/// * `key` - A borrowed `str` that will be used as a key to find a function in
+///           a map.
+/// * `function_map` - A `HashMap` where `String` keys are paired with functions
+///                    as values.
 /// 
 /// # Return value
 /// 
-/// If a function is successfully executed `true` is returned, otherwise `false`
-/// is returned.
-fn get_selection(selection: Option<&str>) -> Option<fn()> {
-    // TODO(JBierenbroodspot): Find a way to store which module to use so it can
-    //                         be ran outside of the main loop.
-    // String returned by `read_line()` include a trailing newline character.
-    match selection.map(|s| s.trim()) {
-        Some("1") => {return Some(hello_world_but_in_another_file::run_self);},
-        Some("2") => {return Some(guess_the_number::run_self);},
-        _   => return None
-    }
+/// Returns a borrowed function if the key exists within the map, otherwise 
+/// `None` is returned.
+fn get_from_fn_map<'a>(key: Option<&str>, 
+                       function_map: &'a FunctionMap) -> Option<&'a fn()> {  
+    // TODO(JBierenbroodspot): Remove trimming.     
+    return key.map(|s| s.trim())
+                    .and_then(|s| function_map.get(s))
 }
 
 fn main() -> io::Result<()> {
     let stdin: io::Stdin = io::stdin();
 
     let mut user_input: String;
-    let mut app: Option<fn()> = None;
+    let mut app: Option<&fn()> = None;
+
+    let mut function_map: FunctionMap = collections::HashMap::new();
+    function_map.insert("1".to_string(), hwbiaf);
+    function_map.insert("2".to_string(), guess_the_number::run_self);
+
     
     while app.is_none() {
         user_input = String::new();
@@ -47,7 +57,8 @@ fn main() -> io::Result<()> {
                    // of type `Some(T)`. Otherwise it id ignored and the value
                    // of the previous statement will be returned, which will be
                    // `None`.
-                   .and(get_selection(Some(&user_input)));
+                   .and(get_from_fn_map(Some(&user_input), 
+                                           &function_map));
     }
 
     if let Some(some_app) = app {
